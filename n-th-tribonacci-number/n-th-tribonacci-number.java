73576af06a36e3bf0d82ceaf2dfb90e97d2b91f6
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2021-09-24 10:50:33 -0700

    Time: 0 ms (100.00%), Space: 35.7 MB (78.77%) - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int tribonacci(int n) {
+        //simple dp
+        
+        if(n <= 1) return n;
+        
+        int[] trib = new int[n + 1];
+        trib[0] = 0;
+        trib[1] = 1;
+        trib[2] = 1;
+                
+        for(int i = 3; i <= n; i++)
+        {
+            trib[i] = trib[i - 1] + trib[i - 2] + trib[i - 3];
+        }
+        
+        return trib[n];
+    }
+}
