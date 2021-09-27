622d6f2c7f34dc73db7957236168cfe2869ca040
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2021-09-26 20:08:39 -0700

    Time: 0 ms (100.00%), Space: 36.5 MB (49.53%) - LeetHub

---
+++ house-robber/house-robber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        //dynamic programming
+        //create new array to store values
+        // set [0] to 0
+        // set [1] to nums[0]
+        //loop through starting at 1
+        // dp[i + 1] = MAthmax(dp[i], dp[i-1] + nums[i]);
+        // return dp[nums.length]
+        
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for(int i = 1; i < nums.length; i++)
+        {
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+        
+        return dp[nums.length];
+    }
+}
