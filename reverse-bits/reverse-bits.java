2cfa5030d6642eee297aa5b90af48d4ec9505765
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2021-09-28 17:32:39 -0700

    Time: 0 ms (100.00%), Space: 38.9 MB (49.93%) - LeetHub

---
+++ reverse-bits/reverse-bits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        //we need to loop over 32 bits
+        
+        int ans = 0;
+        
+        for(int i = 0; i < 32; i++)
+        {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        
+        return ans;
+    }
+}
