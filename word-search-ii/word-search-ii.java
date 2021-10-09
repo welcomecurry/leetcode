751223624f12be6f11a7288b341018b4a8ea8320
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2021-10-08 17:33:14 -0700

    Time: 75 ms (81.41%), Space: 37.7 MB (83.37%) - LeetHub

---
+++ word-search-ii/word-search-ii.java
@@ -0,0 +1,66 @@
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        //trie
+        
+        List<String> output = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board[0].length; j++)
+            {
+                dfs(board, i, j, root, output);
+            }
+        }
+        
+        return output;
+    }
+    
+    private void dfs(char[][] board, int i, int j, TrieNode p, List<String> output)
+    {
+        char c = board[i][j];
+        
+        if(c == '#' || p.next[c - 'a'] == null) return;
+        
+        p = p.next[c - 'a'];
+        
+        if(p.word != null) //found word
+        {
+            output.add(p.word);
+            p.word = null; //set to null so we dont duplicate
+        }
+        
+        board[i][j] = '#';
+        
+        if(i > 0) dfs(board, i - 1, j, p, output);
+        if(j > 0) dfs(board, i, j - 1, p, output);
+        if(i < board.length - 1) dfs(board, i + 1, j, p, output);
+        if(j < board[0].length - 1) dfs(board, i, j + 1, p, output);
+        
+        board[i][j] = c;
+    }
+    
+    private TrieNode buildTrie(String[] words)
+    {
+        TrieNode root = new TrieNode();
+        
+        for(String word : words)
+        {
+            TrieNode p = root;
+            
+            for(char c : word.toCharArray())
+            {
+                if(p.next[c - 'a'] == null) p.next[c - 'a'] = new TrieNode();
+                p = p.next[c - 'a'];
+            }
+            
+            p.word = word;
+        }
+        
+        return root;
+    }
+    
+    class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word;
+    }
+}
