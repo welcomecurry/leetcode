b45e5a4f4b5bba7306929fa0fb5b1e17f185a9e8
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2021-11-07 17:12:29 -0800

    Time: 0 ms (100.00%), Space: 35.9 MB (55.36%) - LeetHub

---
+++ unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numTrees(int n) {
+        //catalan numbers
+        
+        int[] dp = new int[n + 2];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        
+        return dp[n];
+    }
+}
