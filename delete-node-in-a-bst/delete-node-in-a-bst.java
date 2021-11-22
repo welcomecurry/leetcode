1db517d07e0e5345b3775e4092ba4549a56ad588
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2021-11-22 10:43:54 -0800

    Time: 0 ms (100.00%), Space: 39.6 MB (41.74%) - LeetHub

--- delete-node-in-a-bst/delete-node-in-a-bst.java
+++ delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -17,11 +17,11 @@ class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root == null) return root;
         
-        if(key < root.val)
+        if(key < root.val) //recurse left
         {
             root.left = deleteNode(root.left, key);
         }
-        else if(key > root.val)
+        else if(key > root.val) //recurse right
         {
             root.right = deleteNode(root.right, key);
         }
