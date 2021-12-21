b450ecb0a6634dbc7bd8a76f1df4a5693e763b55
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2021-12-20 21:53:09 -0800

    Time: 1 ms (99.97%), Space: 36.2 MB (40.36%) - LeetHub

--- power-of-two/power-of-two.java
+++ power-of-two/power-of-two.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         //check if right most bit is set, if no return true
-        
         if(n <= 0) return false;
         
         return (n & (n - 1)) == 0;
