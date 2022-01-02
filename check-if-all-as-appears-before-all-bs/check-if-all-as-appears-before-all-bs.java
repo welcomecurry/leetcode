2b690c836c92434411140922eec654f648718cfd
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-01-02 14:31:35 +0000

    Time: 1 ms (20.00%), Space: 39 MB (20.00%) - LeetHub

--- check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
+++ check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean checkString(String s) {
-        //just check if its sorted
-        char[] sorted = s.toCharArray();
-        Arrays.sort(sorted);
-        return Arrays.equals(s.toCharArray(), sorted);
+        //just check there is b before a
+        return !s.contains("ba");
     }
 }
