cfebfa864507d687e11f3dc1d8070d6b1aff636e
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-01-03 00:25:53 +0000

    Time: 4 ms (39.54%), Space: 81.4 MB (15.56%) - LeetHub

--- find-the-town-judge/find-the-town-judge.java
+++ find-the-town-judge/find-the-town-judge.java
@@ -1,21 +1,22 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
+        //treat as directed graph, basically counting the indegrees of each person
         //create an array of everybody
-        //decrement a and increment b
-        //loop through trused and current element == n - 1 return i
+        //decrement ai and increment bi
+        //loop through trusted if current element == n - 1 return i, (indegree is n - 1)
         //return -1 if no judge
         
         int[] isTrusted = new int[n + 1];
         
         for(int[] person : trust)
         {
-            isTrusted[person[0]]--;
-            isTrusted[person[1]]++;
+            isTrusted[person[0]]--; //person trusts someone
+            isTrusted[person[1]]++; //person is trusted by someone
         }
         
         for(int i = 1; i < isTrusted.length; i++)
         {
-            if(isTrusted[i] == n - 1) return i;
+            if(isTrusted[i] == n - 1) return i; //n - 1 since everyone trusts town judge except self
         }
         
         return -1;
