b3a61ee30af2a113e1d6c9ee3434596f14893cac
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-01-04 00:10:52 +0000

    Time: 0 ms (100.00%), Space: 37.5 MB (16.08%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,7 +1,8 @@
 class Solution {
     public int bitwiseComplement(int n) {
         //xor each bit
-        
+        //keep N set to n and keep shifting to right till 0 so we know when to terminate loop
+        //keep xor'ing each bit with mask to complement it and shift it each time
         if(n == 0) return 1;
         
         int N = n, mask = 1;
