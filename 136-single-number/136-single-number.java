8e9190d98554276c7002fa656e81a3fd2ba6f6ff
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-02-14 17:08:18 -0800

    Time: 2 ms (64.57%), Space: 50.4 MB (30.87%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        
+        for(int i : nums)
+        {
+            single ^= i;
+        }
+        
+        return single;
+    }
+}
