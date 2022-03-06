d630003588edd87d0da1a28f9b455c4a379dd42c
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-03-05 17:57:10 -0800

    Time: 0 ms (100.00%), Space: 41.6 MB (25.00%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countOrders(int n) {
+        //dynamic programming
+        //For there were 2*(n-1) services, so we have 2*(n-1)+1 choices for the Pickup.
+        //And now there were 2*n-1 services, so we hava 2*n choices for the Delivery.
+        //Now, the total choices is 2*n*(2*n-1).
+        //Because that delivery(i) is always after of pickup(i), so we need divide 2.
+        //And for every P&D inserting, we wil hava n*(2*n - 1) choices.
+        
+        long count = 1, mod = 1000000007;
+        
+        for(int i = 1; i <= n; i++) 
+        {
+            count = count * (2 * i - 1) * i % mod;
+        }
+        
+        return (int) count;
+    }
+}
