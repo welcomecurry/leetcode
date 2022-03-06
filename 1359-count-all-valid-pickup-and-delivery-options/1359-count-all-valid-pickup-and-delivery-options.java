2a9a6467b4918027d0603e9e4caf990245c90e44
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-03-05 17:57:37 -0800

    Time: 0 ms (100.00%), Space: 41.1 MB (33.41%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -9,10 +9,7 @@ class Solution {
         
         long count = 1, mod = 1000000007;
         
-        for(int i = 1; i <= n; i++) 
-        {
-            count = count * (2 * i - 1) * i % mod;
-        }
+        for(int i = 1; i <= n; i++) count = count * (2 * i - 1) * i % mod;
         
         return (int) count;
     }
