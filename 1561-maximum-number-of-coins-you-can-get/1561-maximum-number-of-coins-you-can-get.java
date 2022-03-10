ae57aa517d529cd8bb25055a81533dd101a91c87
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-03-09 18:03:06 -0800

    Time: 35 ms (61.77%), Space: 75.3 MB (54.61%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxCoins(int[] piles) {
+        //greedy
+        //sort and give bob all the middle values
+        //the first n/3 numbers are bobs and n/3 till the end alternate as MLMLML
+        
+        Arrays.sort(piles);
+        
+        int myPile = 0;
+        
+        for(int i = piles.length / 3; i < piles.length; i += 2)
+        {
+            myPile += piles[i];
+        }
+        
+        return myPile;
+    }
+}
