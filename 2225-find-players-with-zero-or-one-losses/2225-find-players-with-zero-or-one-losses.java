ae7a5e8c1116929360a31f152972638db57bff8a
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-04-07 11:45:36 -0700

    Time: 564 ms (7.99%), Space: 148.5 MB (62.85%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        //loop through and count losses for each player
+        //loop through again, if the losses map does not contain player, add to non lost
+        //loop through losses map and if the value is 1 append oto lost one
+        
+        Arrays.sort(matches, (a,b) -> a[1] - b[1]);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        TreeSet<Integer> noLost = new TreeSet<>();
+        List<Integer> oneLost = new ArrayList<>();
+        List<List<Integer>> output = new ArrayList<>();
+        
+        for(int[] match : matches)
+        {
+            map.put(match[1], map.getOrDefault(match[1], 0) + 1);
+        }
+        
+        for(int[] match : matches)
+        {
+            if(!map.containsKey(match[0])) noLost.add(match[0]);
+        }
+        
+        for(Map.Entry<Integer, Integer> entry : map.entrySet())
+        {
+            if(entry.getValue() == 1) oneLost.add(entry.getKey());
+        }
+        
+        output.add(new ArrayList<>(noLost));
+        output.add(oneLost);
+        
+        return output;
+    }
+}
