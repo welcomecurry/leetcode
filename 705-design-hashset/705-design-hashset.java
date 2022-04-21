d73e2162ff3ea14fe75e0d8fd8b252c5927745ff
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-04-20 17:21:58 -0700

    Time: 17 ms (82.02%), Space: 57.6 MB (33.85%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.java
@@ -0,0 +1,28 @@
+class MyHashSet {
+    Map<Integer, Integer> map;
+    public MyHashSet() {
+        map = new HashMap<>();
+    }
+    
+    public void add(int key) {
+        map.put(key, map.getOrDefault(key, 0) + 1);
+    }
+    
+    public void remove(int key) {
+        if(!map.containsKey(key)) return;
+        
+        map.remove(key);
+    }
+    
+    public boolean contains(int key) {
+        return map.containsKey(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
