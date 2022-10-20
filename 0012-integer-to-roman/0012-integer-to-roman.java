953e1ae1afe4263497902d19590af83867b3cde0
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-10-19 17:01:15 -0700

    Time: 30 ms (11.85%), Space: 46.8 MB (21.02%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String intToRoman(int num) {
+        //init arrays that contain ones, tens, hundreds, thousands, 0th element is empty
+        //return a string of the places appended based on the arrays
+        
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] thousands = {"", "M", "MM", "MMM", "MMMM"};
+        
+        return thousands[num / 1000] + hundreds[(num % 1000) / 100] + tens[(num % 100) / 10] 
+            + ones[num % 10];
+    }
+}
