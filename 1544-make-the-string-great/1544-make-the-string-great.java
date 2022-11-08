6d6f3613fcf9115ca56c1bb4b2059268b8838029
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-11-07 16:44:58 -0800

    Time: 3 ms (93.08%), Space: 42.7 MB (64.62%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String makeGood(String s) {
+        //base check if length is 1 return s
+        //create stringbuilder init with string
+        //loop through while i < sb.length - 1
+        //if Math.abs(currChar - nextChar) == 32 => that means we found pair
+        //delete char at i twice
+        //i is the max of i - 1 and 0 since if we have second case we need to reset
+        //otherwise increment i
+        //return stringbuilder
+        
+        if(s.length() == 1) return s;
+        
+        StringBuilder sb = new StringBuilder(s);
+        int i = 0;
+        
+        while(i < sb.length() - 1)
+        {
+            if(Math.abs(sb.charAt(i) - sb.charAt(i + 1)) == 32) //found pair
+            {
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i);
+                i = Math.max(i - 1, 0);
+            }
+            else
+            {
+                i++;
+            }
+        }
+            
+        return sb.toString();
+    }
+}
