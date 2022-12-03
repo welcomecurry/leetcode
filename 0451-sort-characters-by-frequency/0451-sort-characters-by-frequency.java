45d3f4ba05f0fcb850989833717482f5f14cf75a
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2022-12-02 17:03:52 -0800

    Time: 14 ms (91.69%), Space: 43.2 MB (88.52%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String frequencySort(String s) {
+        //loop through chracters in string and store in hashmap
+        //create a max heap (a,b) map.getb - map.geta
+        //add all values into maxheap
+        //while the heap is not empty
+        //get heap.poll loop through while i < heap.poll add that many chars to new string
+        //return new string
+        
+        Map<Character, Integer> map = new HashMap<>();
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+        
+        for(char c : s.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
+        
+        maxHeap.addAll(map.keySet());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(!maxHeap.isEmpty())
+        {
+            char currLetter = maxHeap.poll();
+            for(int i = 0; i < map.get(currLetter); i++) sb.append(currLetter);
+        }
+        
+        return sb.toString();
+    }
+}
