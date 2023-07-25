bcd239b5e15e2008d1524c857a1613e1787448e1
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2023-07-24 18:21:31 -0600

    Time: 1 ms (15.54%), Space: 57.5 MB (13.57%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(i + 1 < arr.length && arr[i] > arr[i + 1])
+            {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
