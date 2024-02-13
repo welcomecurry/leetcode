cc1098e0ebcab5818a0e34747e73730355715934
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2024-02-12 19:40:19 -0700

    Time: 1 ms (100.00%), Space: 45.6 MB (7.44%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -3,20 +3,17 @@ class Solution {
         //loop through if palidrome return
         //if no palindrome return ""
         
-        for(String word : words)
-        {
+        for(String word : words) {
             if(isPalindrome(word)) return word;
         }
         
         return "";
     }
     
-    private boolean isPalindrome(String word)
-    {
+    private boolean isPalindrome(String word) {
         int i = 0, j = word.length() - 1;
         
-        while(i < j)
-        {
+        while(i < j) {
             if(word.charAt(i++) != word.charAt(j--)) return false;
         }
         
