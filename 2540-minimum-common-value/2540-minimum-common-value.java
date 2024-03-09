2a5d70ef47ec866726eb7dc9c90d544dfbd48b60
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2024-03-08 19:03:11 -0700

    Time: 3 ms (41.11%), Space: 61.9 MB (97.41%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,7 +1,6 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         //while current element < either increment and then ret if nums1 == nums2
-        
         int i = 0, j = 0;
         
         while(i < nums1.length && j < nums2.length && nums1[i] != nums2[j]) {
@@ -12,9 +11,8 @@ class Solution {
             }
         }
         
-        if(i == nums1.length || j == nums2.length) return -1;
-        if(nums1[i] != nums2[j]) return -1;
         
-        return nums1[i];
+        return (i == nums1.length || j == nums2.length) || nums1[i] != nums2[j]
+            ? -1 : nums1[i];
     }
 }
