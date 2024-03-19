f930c5e6174750a00223d21bb332b2d844803c18
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2024-03-19 17:53:04 -0600

    Time: 3 ms (83.64%), Space: 46.3 MB (7.04%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        //create charmap from the task arr
+        //sort them since we want to get most frequent element
+        //init maxVal = last element - 1, which is max element and subtracting 1 since there is no idle after last task
+        //init idleSpots = maxVal * n
+        //loop through charMap starting from end
+        //idleSpots -= min of maxVal and current element
+        //if idlespots < 0 return tasks.length otherwise return idlespots + length
+        
+        int[] charMap = new int[26];
+        
+        for(char c : tasks)
+        {
+            charMap[c - 'A']++;
+        }
+        
+        Arrays.sort(charMap);
+        int maxVal = charMap[25] - 1; //-1 we bc don't have to idle after last task
+        int idleSpots = maxVal * n;
+        
+        for(int i = 24; i >= 0; i--)
+        {
+            idleSpots -= Math.min(maxVal, charMap[i]);
+        }
+        
+        return idleSpots < 0 ? tasks.length : idleSpots + tasks.length;
+    }
+}
