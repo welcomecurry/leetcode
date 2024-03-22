5486ef9162b171ec2f326d541a430eb25495b95f
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2024-03-21 18:49:43 -0600

    Time: 7 ms (37.91%), Space: 55.9 MB (96.62%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -5,14 +5,12 @@ class Solution {
         
         ArrayList<Integer> store = new ArrayList<>();
         
-        // for storing the value of linkedlist into arraylist
         ListNode curr = head;
         while(curr != null){
             store.add(curr.val);
             curr = curr.next;
         }
         
-        // for checking the arraylist to be paliandromic or not
         for(int i = 0; i < store.size() / 2; i++){
             if(store.get(i) != store.get(store.size() - 1 - i)) return false;
         }
