7790b351719e4f10676a847cb8a343518c7ad908
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2024-05-21 12:37:39 -0600

    Time: 1 ms (57.66%), Space: 42.4 MB (85.74%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         //powerset
-        
         List<List<Integer>> output = new ArrayList<>();
         Arrays.sort(nums);
         backtrack(output, new ArrayList<>(), nums, 0);
         return output;
     }
     
-    private void backtrack(List<List<Integer>> output, List<Integer> curr, int[] nums, int index)
-    {
+    private void backtrack(List<List<Integer>> output, List<Integer> curr, int[] nums, int index) {
         output.add(new ArrayList<>(curr));
         
-        for(int i = index; i < nums.length; i++)
-        {            
+        for(int i = index; i < nums.length; i++) {            
             curr.add(nums[i]);
             backtrack(output, curr, nums, i + 1);
             curr.remove(curr.size() - 1);
