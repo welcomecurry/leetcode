d55a7bbbcc19fe7737a5a8cc86991a7c7f669574
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2024-05-30 20:22:52 -0600

    Time: 7 ms (18.56%), Space: 45.4 MB (69.87%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
+        
+        int first = -1, second = -1;
+        
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(first == -1 && entry.getValue() == 1) first = entry.getKey();
+            else if(first != - 1 && entry.getValue() == 1) second = entry.getKey();
+        }
+        
+        return new int[] {first, second};
+    }
+}
