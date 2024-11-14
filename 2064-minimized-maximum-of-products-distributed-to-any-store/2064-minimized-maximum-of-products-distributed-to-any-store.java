dbb7deeb0e98ba0220c7998c296be95fe9cace43
welcomecurry <61104530+welcomecurry@users.noreply.github.com>
2024-11-13 19:29:09 -0700

    Time: 28 ms (81.43%), Space: 56.8 MB (44.98%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,47 +1,17 @@
 class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
         //binary search
-        //loop through and find max of quantities to find right bound
-        //loop while left < right
-        //init x (mid)
-        //if can split quanities given x <= n set right to mid
-        //otherwise set left = mid + 1
-        //return right since that is the minimized max
-        
-        int left = 1, right = findMax(quantities);
-        
-        while(left < right)
-        {
-            int x = left + (right - left) / 2; //mid
-            
-            if(canSplitQuantities(quantities, n, x))
-            {
-                right = x;
-            }
-            else
-            {
-                left = x + 1;
-            }
+        //take range from 1 to max in quantities
+        //while low < high
+        //get mid point
+
+        int low = 1, high = Arrays.stream(quantities).max().getAsInt();
+        while (low < high) {
+            int mid = (low + high) / 2, needed = 0;
+            for (int q : quantities) needed += (q + mid - 1) / mid;
+            if (needed <= n) high = mid;
+            else low = mid + 1;
         }
-        
-        return right;
-    }
-    
-    private int findMax(int[] quant)
-    {
-        int max = 0;
-        
-        for(int q : quant) max = Math.max(max, q);
-        
-        return max;
-    }
-    
-    private boolean canSplitQuantities(int[] quantities, int n, int x)
-    {
-        int total = 0;
-        
-        for(int q : quantities) total += q % x == 0 ? q / x : q / x + 1;
-        
-        return total <= n;
+        return low;
     }
 }
